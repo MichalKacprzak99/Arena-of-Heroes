0f946ef4da954c009ceeb42436e7427d8b9edc0a
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-04-30 01:30:50 +0200

    clean movement

--- files/player.py
+++ files/player.py
@@ -18,7 +18,6 @@ class Player:
         self.heroes = self.set_starting_pos()
         self.clicked_hero = None
         self.moved_hero = None
-        self.done_move = False
 
     def set_starting_pos(self):
         if self.player_id == 1:
@@ -28,26 +27,3 @@ class Player:
 
     def move(self, new_pos):
         self.heroes[self.clicked_hero].pos = new_pos
-        self.done_move = True
-
-    def handle_action(self, opponent):
-        run = True
-        while self.done_move is False and run:
-            event = pg.event.wait()
-            if event.type == pg.QUIT:
-                run = False
-                pg.quit()
-            if event.type == pg.MOUSEBUTTONUP:
-                pos = pg.mouse.get_pos()
-                self.clicked_hero = check_clicked_hero(pos, self.heroes)
-                print(self.clicked_hero)
-                if self.clicked_hero:
-                    second_event = pg.event.wait()
-                    if second_event == pg.MOUSEBUTTONUP:
-                        pos = pg.mouse.get_pos()
-                        print(get_tile_pos(pos))
-                        self.move(get_tile_pos(pos))
-                        self.moved_hero = self.heroes[self.clicked_hero]
-                        self.clicked_hero = None
-                        return ["move", self.player_id, self.moved_hero]
-        return None
