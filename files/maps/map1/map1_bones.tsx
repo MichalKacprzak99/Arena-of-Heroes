54262b367a68a6adb4e6793f467c269c9750f42f
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-05-02 10:39:56 +0200

    add files required to create map1

---
+++ files/maps/map1/map1_bones.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="map1_bones" tilewidth="64" tileheight="32" tilecount="1" columns="1">
+ <image source="bones.png" width="64" height="32"/>
+</tileset>
---
+++ files/maps/map1/map1_decor_6.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="map1_decor_6" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="decor_6.png" width="64" height="64"/>
+</tileset>
