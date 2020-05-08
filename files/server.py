a20c00eb3335e0f9d1f6cce4255f5412f45c5efb
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-05-08 11:19:31 +0200

    play with server.py

--- files/server.py
+++ files/server.py
@@ -4,17 +4,17 @@ from game import Game
 from player import Player
 from _thread import start_new_thread
 
-server = "127.0.0.1"
-port = 5556
+
+port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    s.bind((server, port))
+    s.bind(('', port))
 except socket.error as e:
     str(e)
 
-s.listen()
+s.listen(5)
 print("Waiting for a connection, Server Started")
 games = {}
 id_count = 0
