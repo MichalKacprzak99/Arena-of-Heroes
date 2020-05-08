258b91dc2e3f563c94e0e5729c8494c2e038a783
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-05-08 11:22:25 +0200

    better names of variable

--- files/image_flipper.py
+++ files/image_flipper.py
@@ -3,10 +3,10 @@ import pygame as pg
 from os import path
 
 
-def flip_image(filename, name_of_flipped_image):
+def flip_image(image_to_flipped, name_of_flipped_image):
     game_folder = path.dirname(__file__)
     image_folder = path.join(game_folder, 'image')
-    image_to_flip = pg.image.load(load_image(filename))
+    image_to_flip = pg.image.load(load_image(image_to_flipped))
     flipped_image = pg.transform.flip(image_to_flip, True, False)
     save_to = path.join(image_folder, str(name_of_flipped_image))
     pg.image.save(flipped_image, save_to)
