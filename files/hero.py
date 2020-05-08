655d335a37ad7c5f09233b613841ae1931586212
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-05-08 17:15:41 +0200

    add range of healing to Healer

--- files/hero.py
+++ files/hero.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 class Hero:
     def __init__(self, hero_id, pos, attack=10, defense=10, move_range=5, hp=60, max_hp=100, side="east", name="HERO"):
         self.hero_id = hero_id
@@ -17,11 +20,12 @@ class Healer(Hero):
     def __init__(self, hero_id, pos, side="east"):
         super().__init__(hero_id, pos, 5, 5, 3, 75, 75, side=side, name="HEALER")
         self.healing = 30
+        self.healing_range = 2
 
     def special_skill(self, player, *args):
         opponent, object_tiles, clicked_pos = args
         hero_to_heal = player.clicked_own_hero(clicked_pos)
-        if hero_to_heal:
+        if hero_to_heal and self.in_range_of_healing(clicked_pos):
             hero_to_heal.stats["HP"] += self.healing
             if hero_to_heal.stats["HP"] > hero_to_heal.stats["MAX_HP"]:
                 hero_to_heal.stats["HP"] = 100
@@ -30,3 +34,7 @@ class Healer(Hero):
             return ["heal", player.player_id, hero_to_heal]
         else:
             return False
+
+    def in_range_of_healing(self, clicked_pos):
+        distance = sqrt(sum([(i - j) ** 2 for i, j in zip(clicked_pos, self.pos)]))
+        return int(distance) <= self.healing_range
