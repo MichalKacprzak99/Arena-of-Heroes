5530c30576fb97190a7a6b0661653ff6692d0c56
mikolajczykb <61835709+mikolajczykb@users.noreply.github.com>
2020-05-18 18:06:18 +0200

    fixed some bugs

--- files/login.py
+++ files/login.py
@@ -18,6 +18,8 @@ class LoginScreen:
         }
         self.text_info = ["Welcome to Arena of Heroes!", " "]
         self.texts = []
+        self.cover_boxes = []
+        self.inserter_move_flag = False
         self.inserters = ["login", 0, "password", 0]
         self.state = []
         self.background = None
@@ -74,9 +76,13 @@ class LoginScreen:
                     self.texts[1].set_text("Something wrong happened when trying to create your account. Try again.")
 
     def main_menu(self):
+        self.reset_inserter_value()
         self.hide_buttons()
+        self.hide_inserters()
         self.buttons[0].set_visible(1)
         self.buttons[1].set_visible(1)
+        self.buttons[0].set_active(1)
+        self.buttons[1].set_active(1)
 
         self.background.blit()
         self.background.update()
@@ -85,6 +91,8 @@ class LoginScreen:
         self.hide_buttons()
         self.buttons[2].set_visible(1)
         self.buttons[3].set_visible(1)
+        self.buttons[2].set_active(1)
+        self.buttons[3].set_active(1)
         self.texts[0].set_text("Login")
         self.show_inserters()
 
@@ -95,26 +103,35 @@ class LoginScreen:
         self.hide_buttons()
         self.buttons[2].set_visible(1)
         self.buttons[3].set_visible(1)
+        self.buttons[2].set_active(1)
+        self.buttons[3].set_active(1)
         self.texts[0].set_text("Create a new account!")
         self.show_inserters()
+
         self.background.blit()
         self.background.update()
 
     def hide_buttons(self):
         for button in self.buttons:
             button.set_visible(0)
+            button.set_active(0)
+
+    def hide_inserters(self):
         for inserter in self.inserters:
             inserter.set_visible(0)
+        for cover_box in self.cover_boxes:
+            cover_box.set_visible(1)
 
     def reset_inserter_value(self):
         for inserter in self.inserters:
-            inserter._inserted = " "
+            inserter._value = ""
+            inserter._inserted = ""
 
     def show_inserters(self):
         for inserter in self.inserters:
             inserter.set_visible(1)
-
-
+        for cover_box in self.cover_boxes:
+            cover_box.set_visible(0)
 
     def create(self):
         self.state.append("main_menu")
@@ -126,8 +143,9 @@ class LoginScreen:
             text_to_add = self.inserters[index]
             self.inserters[index] = thorpy.make_text(text_to_add)
             self.inserters[index+1] = (thorpy.Inserter(value="", size=(200, 20), quit_on_click=False, finish=True))
+            self.cover_boxes    .append(thorpy.Image(path = None, color = (168, 139, 50)))
         self.background = thorpy.Background(color=(168, 139, 50), image=None,
-                                            elements=self.buttons+self.texts+self.inserters)
+                                            elements=self.buttons+self.texts+self.inserters+self.cover_boxes)
         thorpy.store(self.background, self.buttons[0:2], x=game_settings["GAME_SCREEN_WIDTH"]/2,
                      y=game_settings["GAME_SCREEN_HEIGHT"]/2 - 150, align="center")
         thorpy.store(self.background, self.buttons[2:4], x=game_settings["GAME_SCREEN_WIDTH"]/2,
@@ -138,6 +156,8 @@ class LoginScreen:
                      y=game_settings["GAME_SCREEN_HEIGHT"] / 2 - 200, align="center")
         thorpy.store(self.background, [self.texts[1]], x=game_settings["GAME_SCREEN_WIDTH"] / 2 - 200,
                      y=game_settings["GAME_SCREEN_HEIGHT"] / 2 + 100, align="left")
+        thorpy.store(self.background, self.cover_boxes, x=game_settings["GAME_SCREEN_WIDTH"] / 2 - 60,
+                     y=game_settings["GAME_SCREEN_HEIGHT"] / 2 - 77, align="center")
         self.menu = thorpy.Menu(self.background)
         for element in self.menu.get_population():
             element.surface = self.window
@@ -150,12 +170,12 @@ class LoginScreen:
             clock.tick(60)
             current_login_window = self.state[-1]
             self.draw_window[current_login_window]()
-
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
-
                 self.menu.react(event)
+
+
 
 
 
