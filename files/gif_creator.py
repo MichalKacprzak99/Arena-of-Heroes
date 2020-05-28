42e9965f2fe1a72b7579cd9e340f3f42ef29c318
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-05-28 22:10:11 +0200

    update README.md

--- files/gif_creator.py
+++ files/gif_creator.py
@@ -1,9 +1,9 @@
 from moviepy.editor import VideoFileClip, CompositeVideoClip
 
-clip = (VideoFileClip("D:\Documents\studia\semestr4\Arena-of-Heroes\demo\\attack.mp4")
-        .subclip((0, 6.1), (0, 6.8))
+clip = (VideoFileClip("D:\Documents\studia\semestr4\Arena-of-Heroes\demo\\move.mp4")
+        .subclip((0, 5.8), (0, 8))
         .speedx(0.4)
-        .crop(x1=611, y1=485, x2=800, y2=660)
+        .crop(x1=290, y1=236, x2=552, y2=563)
         )
 # clip.show(interactive=True)
 d = clip.duration
@@ -12,5 +12,5 @@ snapshot = (clip.to_ImageClip()
             .crossfadein(d/6)
             .set_start(5*d/6))
 composition = CompositeVideoClip([clip, snapshot])
-composition.write_gif('carry.gif', fps=clip.fps, fuzz=3)
-composition.write_gif("D:\Documents\studia\semestr4\Arena-of-Heroes\demo\\attack.gif")
+composition.write_gif('carry.gif', fps=clip.fps, fuzz=0)
+composition.write_gif("D:\Documents\studia\semestr4\Arena-of-Heroes\demo\\move.gif")
